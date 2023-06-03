9b788e218dbdfbe269da80c3dae89f87616cd35d
Ray Haddad <rayhaddad@globalistic.tech>
2023-06-03 18:44:04 +0300

    mobile footer

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
 
       <body className="overflow-y-auto">
         <Navbar />
-        <main className="min-h-[calc(100vh-100px)]">{children}</main>
+        <main className="sm:min-h-[calc(100vh-100px)] min-h-[calc(100vh-200px)]">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
