6f9bb092365d0b6e3ebb2bf7eec0ddba533a07f9
Ray Haddad <rayhaddad@globalistic.tech>
2023-06-07 22:15:04 +0300

    try 1

--- portfolio/src/app/resume/page.tsx
+++ portfolio/src/app/resume/page.tsx
@@ -1,9 +1,15 @@
+// "use client";
+import { useEffect, useMemo, useState, createContext } from "react";
+import { pdfjs, Document, Page } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 export default function Resume() {
-  const pdfUrl = "./RayHaddad.pdf";
+  const pdfUrl = useMemo(() => "./RayHaddad.pdf", []);
   return (
     <section>
       <h1 className="text-center sm:text-4xl text-lg mb-5">My Resume</h1>
-      <iframe src={pdfUrl} width="100%" height="500px" title="Resume" />
+      <Document file="./RayHaddad.pdf">
+        <Page pageIndex={0} />
+      </Document>
       <a
         className="text-gray-600 mb-5 text-end w-full block mt-5"
         download
